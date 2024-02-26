6d18dbc0e10e0f1520ebe6f9ec8552e2aaff5d4d
unknown <theopinheiro12@gmail.com>
2024-02-26 16:43:09 -0400

    feito a interface da area de cadastrar produtos

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,30 @@
+import style from "./create.module.scss";
+
 export default function Create() {
   return (
-    <>
-      <p>pagina de cadastrar produtos</p>
-    </>
+    <div className={style.container}>
+      <h1>Cadastrar um produto novo!</h1>
+
+      <div className={style.nameProduct}>
+        <span>Nome: </span> <input type="text" placeholder="Nome do produto!" />
+      </div>
+      <div className={style.priceProduct}>
+        <span>Preço: </span>{" "}
+        <input type="number" placeholder="valor do seu produto em centavos" />
+      </div>
+      <div className={style.descriptionProduct}>
+        <span>Descrição: </span>
+        <input
+          type="text"
+          placeholder="Fale um pouco sobre seu produto e dos beneficios dele"
+        />
+      </div>
+      <div>
+        <button>
+          <span>Cadastrar Produto</span>
+          <i></i>
+        </button>
+      </div>
+    </div>
   );
 }
